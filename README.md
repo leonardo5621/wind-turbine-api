581dd61341c05db623eadbdbf7e8751562b1f11c
leonardo5621 <leonardo5621@gmail.com>
2023-11-30 10:07:26 -0300

    API in docker compose

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# wind-turbine-api
+# Wind Turbines API
+
+Project of wind turbine data API. It has been written in Python using FastAPI and SQLAlchemy as the ORM to interact with a postgres database
--- app/services/sessionmaker.py
+++ app/services/sessionmaker.py
@@ -1,8 +1,18 @@
+import os
+from dotenv import load_dotenv
 from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-db = create_engine("postgresql+psycopg2://root:password@localhost:5431/turbines")
+load_dotenv()
+
+db_user = os.environ.get("DB_USER", "root")
+db_password = os.environ.get("DB_PASSWORD", "password")
+db_host = os.environ.get("DB_HOST", "localhost")
+db_port = os.environ.get("DB_PORT", "5432")
+db_name = os.environ.get("DB_NAME", "turbines")
+
+db = create_engine("postgresql+psycopg2://{}:{}@{}:{}/{}".format(db_user, db_password, db_host, db_port, db_name))
 
 def get_session_dependency() -> Generator:
     with Session(db) as session:
